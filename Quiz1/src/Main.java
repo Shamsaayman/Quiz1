268d47684198217f78368e533f9e64d0c6a7eb50
Shamsaayman <91327556+Shamsaayman@users.noreply.github.com>
2024-02-19 10:35:36 +0300

    Add files via upload

---
+++ Quiz1/src/Main.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+/////////////////////NUMBER 1 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//        System.out.println("Enter First Number");
+//        int n1 = s.nextInt();
+//        System.out.println("Enter Second Number");
+//        int n2 = s.nextInt();
+//        System.out.println("Enter Third Number");
+//        int n3 = s.nextInt();
+//
+//        if (n1>=n2 && n1>=n3){
+//            System.out.println(n1 + " Is the largest Number");
+//        }
+//        else if (n2>=n1 && n2>=n3){
+//            System.out.println(n2 + " Is the largest Number");
+//        }
+//        else{
+//            System.out.println(n3 + " Is the largest Number");
+//        }
+/////////////////////NUMBER 2 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//        System.out.println("Enter a String");
+//        String str = s.next();
+//        System.out.println("Enter Index Number");
+//        int n = s.nextInt();
+//        System.out.println("The letter at given index is : "+ str.charAt(n));
+
+/////////////////////NUMBER 3 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//        int sum = 0 ;
+//        int n ;
+//        do{
+//            System.out.println("Enter a Number and 0 to exit");
+//             n = s.nextInt();
+//             sum = sum + n ;
+//        }
+//        while (n!=0);
+//        System.out.println("The sum of the numbers you entered is: " + sum);
+
+/////////////////////NUMBER 4 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//        int [] num = {10 , -21 , 30 , 31, -25};
+//        for (int i = 0; i < num.length; i++) {
+//            if (num[i]>0){
+//                System.out.println(num[i] + " is a Positive Number ");
+//            }
+//            else{
+//                System.out.println(num[i] + " is a Negative Number ");
+//            }
+//        }
+/////////////////////NUMBER 5 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// String [] names = {"Tuwaiq" , "Bootcamp", "Student" , "JAVA"};
+//
+//            if (names[0].length()<names[1].length() && names[0].length()<names[2].length() && names[0].length()<names[3].length()){
+//            System.out.println("The Shortest Word is: " + names[0]);
+//       }
+//            else if (names[1].length()<names[0].length() && names[1].length()<names[2].length() && names[1].length()<names[3].length()){
+//                System.out.println("The Shortest Word is: " + names[1]);
+//            }
+//            else if (names[2].length()<names[0].length() && names[2].length()<names[1].length() && names[2].length()<names[3].length()){
+//                System.out.println("The Shortest Word is: " + names[2]);
+//            }
+//            else{
+//                System.out.println("The Shortest Word is: " + names[3]);
+//            }
+        }
+        }
+
